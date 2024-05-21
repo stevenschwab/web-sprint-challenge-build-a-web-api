2f50f11329b7c7543c83d0b8d90a8066c79c68bc
Steve Schwab <schwabs@umich.edu>
2024-05-21 14:38:32 -0700

    `[GET] /api/projects/:id` Returns a project with the given `id` as the body of the response. If there is no project with the given `id` it responds with a status code 404.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -11,6 +11,19 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+    Projects.get(id)
+        .then(project => {
+            if (project) {
+                res.json(project)
+            } else {
+                res.status(404).json({ message: "Project not found!" })
+            }
+        })
+        .catch(next)
+})
+
 router.use((error, req, res, next) => {
     res.status(error.status || 500).json({
         message: error.message,
